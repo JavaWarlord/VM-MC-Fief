48a5eb19783733dbb12ae6f0f314319098737a6c
JavaWarlord <JavaWarlord@gmail.com>
2013-12-26 16:48:18 -0500

    Just commiting to push

--- VM-MC-Fief/src/Fiefdom.java
+++ VM-MC-Fief/src/Fiefdom.java
@@ -2,9 +2,9 @@ import org.bukkit.Material;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.valiantmarauders.minecraft.block.CuboidSelectionManager;
 import com.valiantmarauders.minecraft.block.PlayerSelectBlockListener;
-import com.valiantmarauders.minecraft.block.SelectionManager;
+import com.valiantmarauders.minecraft.selection.CuboidSelectionManager;
+import com.valiantmarauders.minecraft.selection.SelectionManager;
 
 public class Fiefdom extends JavaPlugin {
 	PluginManager pm;
@@ -12,10 +12,11 @@ public class Fiefdom extends JavaPlugin {
 
 	public void onEnable() {
 		pm = this.getServer().getPluginManager();
-		sm = new CuboidSelectionManager(Material.WOOD_SWORD);
+		sm = new CuboidSelectionManager(this, Material.WOOD_SPADE);
 		pm.registerEvents(new PlayerSelectBlockListener(this, sm), this);
 	}
 
 	public void onDisable() {
+		// write everything to db
 	}
 }
